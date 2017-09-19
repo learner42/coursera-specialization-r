21a052332b1afe49b84cc41328e97ca1ac476566
Coursera Learner 42 <coursera.learner42@gmail.com>
2017-09-19 12:50:56 +0000

    Document the functions.

--- r-packages/peer1/fars_function.R
+++ r-packages/peer1/fars_function.R
@@ -1,3 +1,19 @@
+#' Read FARS csv data
+#'
+#' This function reads FARS (Fatality Analysis Reporting System) data from a csv input and returns a
+#' data frame tbl (thin wrapper around data.frame) object
+#'
+#' @param filename A csv path
+#' @return A data frame tbl object.
+#'
+#' @details If the input file does not exists, an error will be thrown with the message
+#'     "file <requested_file> does not exists"
+#'
+#' @importFrom readr::read_csv dplyr::tbl_df
+#'
+#' @examples
+#'   fars_read("data/my_fars_data.csv")
+#' @export
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
@@ -7,17 +23,44 @@ fars_read <- function(filename) {
         dplyr::tbl_df(data)
 }
 
+#' Make the filename for the fars data for a given year
+#'
+#' @param year A year to construct the csv filename
+#' @return The csv filename for the requested year
+#'
+#' @details If the paramer year cannot be cast to an integer, the name accident_NA.csv.bz2 will be
+#'     returned
+#'
+#' @examples
+#'   make_filename(2007)
+#'
+#' @export
 make_filename <- function(year) {
         year <- as.integer(year)
         sprintf("accident_%d.csv.bz2", year)
 }
 
+#' Read the available year, month from the dataset
+#'
+#' This function reads the available month, year combinations from the dataset
+#'
+#' @param years The years to be read
+#' @return A list of data frames corresponding to the requested years. Each data frame contains
+#'     column: MONTH and year, indicating the avaiable year-month from the data
+#'
+#' @details If a year results in error (file not available, corrupted data, etc) a warning will be
+#'     shown and a NULL object is returned
+#' @importFrom dplyr::mutate dplyr::select
+#' @examples
+#'   fars_read_years(c(2013,2014))
+#'
+#' @export
 fars_read_years <- function(years) {
         lapply(years, function(year) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -26,14 +69,48 @@ fars_read_years <- function(years) {
         })
 }
 
+#' Summarize the data for given years
+#'
+#' This function takes some years as input and return a dataframe showing the number of records per
+#' each year
+#'
+#' @param years The years to be read
+#' @return A data frame of one row, and as many columns as the requested years, the cell represents
+#'     the number of records for the given year
+#'
+#' @importFrom dplyr::bind_rows dplyr::group_by dplyr::summarize tidyr::spread
+#' @examples
+#'   fars_summarize_years(c(2013,2014)
+#'   The result should look like
+#'
+#'         2013    2014
+#'  n     97987  121313
+#' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
 
+#' Map the FARS data for a state in a given year
+#'
+#' This functions map the accident data for a state-year combination
+#'
+#' @param state.num An integer representing the state ID
+#' @param year A year
+#' @return A map object or NULL if there is no accidents to report
+#'
+#' @details
+#'   If the state.num is invalid an error will be thrown specifying this
+#'   If there is no data associated with the state, a message "no accidents to plot" will be shown,
+#'     a invisible NULL is returned
+#'   If there is some data, points representing where the accidents occur is shown on a map
+#'
+#' @importFrom maps::map graphics::points
+#' @export
+#'
 fars_map_state <- function(state.num, year) {
         filename <- make_filename(year)
         data <- fars_read(filename)
